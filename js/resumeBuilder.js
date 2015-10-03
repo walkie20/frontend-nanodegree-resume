d5e43901ff051d6a5534752c080ce329725e3e50
walkie20 <walkiephone@gmail.com>
2015-10-03 00:48:33 -0400

    Messed some with JS

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,22 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+ $("#main").append("Zachary Nicholas");
+var firstname = "Zachary";
+var age = 32;
+var awesomethoughts = "I am Spartucus and I am awesome";
+console.log(firstname);
+console.log(awesomethoughts);
+var email = "zacharytnicholas@gmail.com";
+
+var funthoughts = awesomethoughts.replace ("awesome", "fun");
+console.log(funthoughts);
+
+var formattedname = HTMLheaderName.replace ("%data%", name);
+var name = "Zachary Nicholas"
+var role = "Multimedia Designer";
+
+var formattedrole = HTMLheaderRole.replace ("%data%", role);
+
+$ ("#header").prepend(formattedrole);
+$ ("#header").prepend(formattedname);
